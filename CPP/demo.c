343fdf357f8e22e064fb79c10ea723ea0332988a
nancy1016 <liukaixa@163.com>
2019-02-25 23:10:19 -0500

    test

---
+++ CPP/demo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Add(int a,int b);
+
+int main()
+{
+    Add(1,2);
+    system("pauae");
+    return 0;
+}
---
+++ CPP/demo.s
@@ -0,0 +1,29 @@
+	.file	"demo.c"
+	.section	.rodata
+.LC0:
+	.string	"pauae"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$2, %esi
+	movl	$1, %edi
+	call	Add
+	movl	$.LC0, %edi
+	call	system
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-28)"
+	.section	.note.GNU-stack,"",@progbits
